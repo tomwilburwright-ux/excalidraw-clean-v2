4b6b55a264a0c3a3589ebccdee56bbd4f7be9fa7
tomwilburwright-ux <tomwilburwright@gmail.com>
2026-05-04 15:05:15 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
+import React from "react";
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html>
+    <html lang="en">
       <body>{children}</body>
     </html>
   );
